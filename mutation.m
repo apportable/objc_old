7fe04be612008a6d21ebaef820ac130051461a8a
Adam Barth <abarth@apportable.com>
2011-01-09 19:51:42 -0800

    (split)Remove gdb-only attribute
    DCF:  9aff7a816d3714664fdfba6fcc23e9a634dedd09

--- mutation.m
+++ mutation.m
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// This function is exported as a weak symbol to enable GNUstep or some other
-// framework to replace it trivially
-void __attribute__((weak)) objc_enumerationMutation(void *obj)
+void objc_enumerationMutation(void *obj)
 {
 	fprintf(stderr, "Mutation occured during enumeration.");
 	abort();
