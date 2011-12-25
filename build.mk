3326584ff6c71214c911b911a82063ce6dc08ea6
Marc Salem <salem@apportable.com>
2011-12-25 11:13:35 -0800

    (split)Checkpoint, libobjc.so compiles
    DCF:  675aa052b16cd5f0d483699ce33f4ec1a840d5ef

--- build.mk
+++ build.mk
@@ -21,30 +21,31 @@ CCFLAGS += \
 endif
 
 OBJECTS = \
-	NSBlocks.o \
-	Protocol2.o \
-	abi_version.o \
-	blocks_runtime.o \
-	caps.o \
-	category_loader.o \
-	class_table.o \
-	dtable.o \
-	eh_personality.o \
-	encoding2.o \
-	hash_table.o \
-	hooks.o \
-	ivar.o \
-	legacy_malloc.o \
-	loader.o \
-	mutation.o \
-	properties.o \
-	protocol.o \
-	runtime.o \
-	sarray2.o \
-	selector_table.o \
-	sendmsg2.o \
-	statics_loader.o \
-	sync.o \
-	toydispatch.o \
+
+	# NSBlocks.o \
+	# Protocol2.o \
+	# abi_version.o \
+	# blocks_runtime.o \
+	# caps.o \
+	# category_loader.o \
+	# class_table.o \
+	# dtable.o \
+	# eh_personality.o \
+	# encoding2.o \
+	# hash_table.o \
+	# hooks.o \
+	# ivar.o \
+	# legacy_malloc.o \
+	# loader.o \
+	# mutation.o \
+	# properties.o \
+	# protocol.o \
+	# runtime.o \
+	# sarray2.o \
+	# selector_table.o \
+	# sendmsg2.o \
+	# statics_loader.o \
+	# sync.o \
+	# toydispatch.o \
 
 include $(ROOTDIR)/module.mk
