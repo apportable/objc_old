09570f8a8cbaba1dbfaf2a367431112e9e83c2ba
Marc Salem <salem@apportable.com>
2012-01-04 14:19:39 -0800

    (split)libfoundation.so working with [app]
    DCF:  742daa897950d8415af9d7062ad4a95282115f6b

--- objc-runtime/jni/objc/mutation.m
+++ objc-runtime/jni/objc/mutation.m
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if !defined(DEBUG_BREAK)
+#if !defined(NDEBUG) && defined(ANDROID)
+#include <android/log.h>
+#define DEBUG_BREAK() { ((void)__android_log_print(ANDROID_LOG_WARN, "objc", "DEBUG_BREAK: Hit breakpoint at %s:%d", __FILE__, __LINE__)); int *debug_break_crash_var = 0; *debug_break_crash_var = 42; }
+#else
+#define DEBUG_BREAK(...)
+#endif
+#endif
+
 void objc_enumerationMutation(void *obj)
 {
 	fprintf(stderr, "Mutation occured during enumeration.");
+  DEBUG_BREAK();
 	abort();
 }
 
