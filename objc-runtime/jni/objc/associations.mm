268f4e336f765728ef3ea57a9566998fdf291f07
Philippe Hausler <philippe@apportable.com>
2012-01-28 16:40:04 -0800

    (split)object association fix for destructors
    DCF:  34475532ffa17beae319d34a69900ca90f90dc39

--- objc-runtime/jni/objc/associations.mm
+++ objc-runtime/jni/objc/associations.mm
@@ -14,77 +14,94 @@ typedef std::map<const void *, id> ObjCBinding;
 typedef std::map<id, ObjCPolicy> ObjCPolicyMap;
 typedef std::map<id, ObjCBinding> ObjCBindingMap;
 
-static ObjCPolicyMap policies;
-static ObjCBindingMap associations;
+static int __objc_finalizers_imminent = 0; 
 
-extern "C" int __objc_finalizers_imminent = 0; 
+class ObjCAssociationMap {
+private:
+	ObjCPolicyMap policies;
+	ObjCBindingMap associations;
+public:
+	~ObjCAssociationMap() {
+		__objc_finalizers_imminent = 1; // helps to avoid a race condition on finalizers
+	}
+	
+	void set(id object, const void *key, id value, objc_AssociationPolicy policy) {
+	    switch(policy)
+	    {
+	        case OBJC_ASSOCIATION_ASSIGN:
+	            associations[object][key] = value;
+	            break;
+	        case OBJC_ASSOCIATION_RETAIN_NONATOMIC:
+	        case OBJC_ASSOCIATION_RETAIN:
+	            if(associations[object][key] != value)
+	            {
+	                [associations[object][key] release];
+	                associations[object][key] = [value retain];
+	            }
+	            break;
+	        case OBJC_ASSOCIATION_COPY_NONATOMIC:
+	        case OBJC_ASSOCIATION_COPY:
+	            if(associations[object][key] != value)
+	            {
+	                [associations[object][key] release];
+	                associations[object][key] = [value copy];
+	            }
+	            break;
+	    }
+    
+	    policies[object][key] = policy;
+		
+	}
+	
+	id get(id object, const void *key) {
+		return associations[object][key];
+	}
+	
+	void remove(id object) {
+	    ObjCBindingMap::iterator bindings = associations.find(object);
+	    if(bindings != associations.end())
+	    {
+	        for(ObjCBinding::iterator it = bindings->second.begin(); it != bindings->second.end(); ++it) 
+	        {
+	            switch(policies[object][it->first])
+	            {
+	                case OBJC_ASSOCIATION_RETAIN_NONATOMIC:
+	                case OBJC_ASSOCIATION_RETAIN:
+	                case OBJC_ASSOCIATION_COPY_NONATOMIC:
+	                case OBJC_ASSOCIATION_COPY:
+	                    [it->second release];
+	                    // fall through
+	                case OBJC_ASSOCIATION_ASSIGN:
+	                    it->second = NULL;
+	                    break;
+	            }
+	        }
+	        bindings->second.clear();
+	        associations.erase(object);
+	        policies.erase(object);
+	    }
+	}
+};
+
+static ObjCAssociationMap store;
 
 void objc_setAssociatedObject(id object, const void *key, id value, objc_AssociationPolicy policy)
 {
-    if(!object || !key)
+    if(!object || !key || __objc_finalizers_imminent)
         return;
-    switch(policy)
-    {
-        case OBJC_ASSOCIATION_ASSIGN:
-            associations[object][key] = value;
-            break;
-        case OBJC_ASSOCIATION_RETAIN_NONATOMIC:
-        case OBJC_ASSOCIATION_RETAIN:
-            if(associations[object][key] != value)
-            {
-                [associations[object][key] release];
-                associations[object][key] = [value retain];
-            }
-            break;
-        case OBJC_ASSOCIATION_COPY_NONATOMIC:
-        case OBJC_ASSOCIATION_COPY:
-            if(associations[object][key] != value)
-            {
-                [associations[object][key] release];
-                associations[object][key] = [value copy];
-            }
-            break;
-    }
-    
-    policies[object][key] = policy;
+    store.set(object, key, value, policy);
 }
 
 id objc_getAssociatedObject(id object, const void *key)
 {
-    if(!object || !key)
+    if(!object || !key || __objc_finalizers_imminent)
         return NULL;
-    return associations[object][key];
+    return store.get(object, key);
 }
 
 void objc_removeAssociatedObjects(id object)
 {
     if(!object || __objc_finalizers_imminent)
         return;
-    // Technically this should be syncrhonized to avoid stepping on the toes of another remove op or another set/get op
-    // but if you are playing with this api, it is the caller's responsability to make sure it is dealt with in a thread
-    // safe manner, else the performance takes a severe dive since this is called on EVERY release of an object
-    
-    ObjCBindingMap::iterator bindings = associations.find(object);
-    if(bindings != associations.end())
-    {
-        DEBUG_LOG("nuking objects");
-        for(ObjCBinding::iterator it = bindings->second.begin(); it != bindings->second.end(); ++it) 
-        {
-            switch(policies[object][it->first])
-            {
-                case OBJC_ASSOCIATION_RETAIN_NONATOMIC:
-                case OBJC_ASSOCIATION_RETAIN:
-                case OBJC_ASSOCIATION_COPY_NONATOMIC:
-                case OBJC_ASSOCIATION_COPY:
-                    [it->second release];
-                    // fall through
-                case OBJC_ASSOCIATION_ASSIGN:
-                    it->second = NULL;
-                    break;
-            }
-        }
-        bindings->second.clear();
-        associations.erase(object);
-        policies.erase(object);
-    }
+    store.remove(object);
 }
