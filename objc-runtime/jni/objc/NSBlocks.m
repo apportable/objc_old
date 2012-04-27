25cc191206bc812aa97283e4fa2206e688ce8657
Philippe Hausler <philippe@apportable.com>
2012-04-26 23:13:10 -0700

    (split)instantiate block runtime bridge classes to dynamically transmute blocks into NSObjects
    DCF:  00987964ef8c6b93c900f1a6f2e99100c4e498e0

--- objc-runtime/jni/objc/NSBlocks.m
+++ objc-runtime/jni/objc/NSBlocks.m
@@ -1,18 +1,25 @@
 #import "objc/runtime.h"
 #import "class.h"
 #import "lock.h"
-#import "objc/blocks_runtime.h"
+#import "blocks/Block_private.h"
 #import "dtable.h"
+#include "objc_debug.h"
 #include <assert.h>
 
-struct objc_class _NSConcreteGlobalBlock;
-struct objc_class _NSConcreteStackBlock;
+static struct objc_class __NSBlock;
 
-static struct objc_class _NSConcreteGlobalBlockMeta;
-static struct objc_class _NSConcreteStackBlockMeta;
-
-static struct objc_class _NSBlock;
+static struct objc_class *_NSBlock = &__NSBlock;
 static struct objc_class _NSBlockMeta;
+
+static struct objc_class _NSConcreteStackBlockMeta;
+static struct objc_class _NSConcreteMallocBlockMeta;
+static struct objc_class _NSConcreteAutoBlockMeta;
+static struct objc_class _NSConcreteFinalizingBlockMeta;
+static struct objc_class _NSConcreteGlobalBlockMeta;
+
+static void *_NSBlockCopyNoZone(void *src, SEL _cmd) {
+	return _Block_copy(src);
+}
 
 static void *_NSBlockCopy(void *src, SEL _cmd, void *zone) {
 	return _Block_copy(src);
@@ -29,36 +36,39 @@ static void *_NSBlockRetain(void *src, SEL _cmd) {
 static void createNSBlockSubclass(Class superclass, Class newClass, 
 		Class metaClass, char *name)
 {
-	// Initialize the metaclass
-	//metaClass->class_pointer = superclass->class_pointer;
-	//metaClass->super_class = superclass->class_pointer;
 	metaClass->info = objc_class_flag_meta;
+	metaClass->super_class = superclass;
+	metaClass->name = strdup(name);
+	metaClass->info = objc_class_flag_meta | objc_class_flag_resolved;
 	metaClass->dtable = __objc_uninstalled_dtable;
 
-	// Set up the new class
 	newClass->isa = metaClass;
-	newClass->super_class = (Class)superclass->name;
-	newClass->name = name;
-	newClass->info = objc_class_flag_class;
+	newClass->super_class = superclass;
+	newClass->name = strdup(name);
+	newClass->info = objc_class_flag_class | objc_class_flag_resolved;
 	newClass->dtable = __objc_uninstalled_dtable;
 
-	LOCK_UNTIL_RETURN(__objc_runtime_mutex);
 	class_table_insert(newClass);
-
 }
 
 #define NEW_CLASS(super, sub) \
-	createNSBlockSubclass(super, &sub, &sub ## Meta, #sub)
+	createNSBlockSubclass(super, sub, &sub ## Meta, #sub)
 
 BOOL objc_create_block_classes_as_subclasses_of(Class super)
 {
-	if (_NSBlock.super_class != NULL) { return NO; }
+	DEBUG_LOG("Initialize block classes!");
 
 	NEW_CLASS(super, _NSBlock);
-	NEW_CLASS(&_NSBlock, _NSConcreteStackBlock);
-	NEW_CLASS(&_NSBlock, _NSConcreteGlobalBlock);
-	class_addMethod(&_NSBlock, sel_registerName("release"), (IMP)&_NSBlockRelease, "v@:");
-	class_addMethod(&_NSBlock, sel_registerName("retain"), (IMP)&_NSBlockRetain, "@@:");
-	class_addMethod(&_NSBlock, sel_registerName("copyWithZone:"), (IMP)&_NSBlockCopy, "@@0:^{_NSZone=^?^?^?^?^?^?^?I@^{_NSZone}}");
+	class_addMethod(_NSBlock, sel_registerName("release"), (IMP)&_NSBlockRelease, "v@:");
+	class_addMethod(_NSBlock, sel_registerName("retain"), (IMP)&_NSBlockRetain, "@@:");
+	class_addMethod(_NSBlock, sel_registerName("copy"), (IMP)&_NSBlockCopyNoZone, "@@:");
+	class_addMethod(_NSBlock, sel_registerName("copyWithZone:"), (IMP)&_NSBlockCopy, "@@0:^{_NSZone=^?^?^?^?^?^?^?I@^{_NSZone}}");
+
+	NEW_CLASS(_NSBlock, _NSConcreteStackBlock);
+	NEW_CLASS(_NSBlock, _NSConcreteMallocBlock);
+	NEW_CLASS(_NSBlock, _NSConcreteAutoBlock);
+	NEW_CLASS(_NSBlock, _NSConcreteFinalizingBlock);
+	NEW_CLASS(_NSBlock, _NSConcreteGlobalBlock);
+
 	return YES;
 }
