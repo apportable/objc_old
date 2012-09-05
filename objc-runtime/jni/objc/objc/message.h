d627b2cad6e9611be2bbdccb010c91acf88a64c1
Zac Bowling <zac@zacbowling.com>
2012-09-05 13:51:35 -0700

    (split)faster startup and missing header
    DCF:  23e7363e5165b6f104a07f45fc8cd30d8fdf49ff

---
+++ objc-runtime/jni/objc/objc/message.h
@@ -0,0 +1,13 @@
+#include <objc/objc.h>
+#include <objc/runtime.h>
+
+
+#ifndef __LIBOBJC_MESSAGE_H_INCLUDED__
+#define __LIBOBJC_MESSAGE_H_INCLUDED__
+
+void objc_msgSend(/* id self, SEL op, ... */ void);
+void objc_msgSend_stret(/* id self, SEL op, ... */ void);
+void objc_msgSend_fpret(/* id self, SEL op, ... */ void);
+
+
+#endif //__LIBOBJC_MESSAGE_H_INCLUDED__
