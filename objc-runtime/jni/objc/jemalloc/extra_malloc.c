3bf188d4440e9f013ab2965cbb77364afda6cfa4
Paul Beusterien <paulb@apportable.com>
2012-09-25 12:38:41 -0700

    (split)Fix broken jemalloc strndup
    DCF:  5ad676bfba2fe893b6087b5177ff839798549123

--- objc-runtime/jni/objc/jemalloc/extra_malloc.c
+++ objc-runtime/jni/objc/jemalloc/extra_malloc.c
@@ -39,7 +39,10 @@ wrap(strndup)(const char *src, size_t len)
 {
   char* dst = (char*)wrap(malloc)(len + 1);
   if (dst)
-    strncpy(dst, src, len + 1);
+  {
+    strncpy(dst, src, len);
+    dst[len] = 0;
+  }
   return dst;
 }
 
